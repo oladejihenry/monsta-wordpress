c96ac7ebc52ad980e8bfc4a00ba4f783e7f2a1cc
Henry Oladeji <officialmonstajamss@gmail.com>
2020-08-18 21:28:42 +0100

    Update

---
+++ sidebar.php
@@ -0,0 +1,101 @@
+<div class="sidebars desktop">
+    <div class="desktop side-ad">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Ads 2 -->
+        <ins class="adsbygoogle"
+            style="display:inline-block;width:300px;height:600px"
+            data-ad-client="ca-pub-8897835908934169"
+            data-ad-slot="3619596871"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
+    <div class="sidebar tracks">
+        <div class="section-header item-header desktop">
+            <div class="title">
+                <h1>Hip Hop
+                    <span>Singles</span>
+                </h1>
+            </div>
+        </div>
+        <ul>
+            <li>
+                <a href="{{ route('posts.show',$post->slug) }}">
+                <div><i class="fa fa-headphones"></i></div>
+                <div><strong>Underworld - </strong>Eminem</div>
+                </a>
+            </li>
+        </ul>
+        <a href="/category/music" class="uni-link">
+            More Singles &nbsp;
+            <i class="fa fa-angle-right"></i>
+        </a>
+    </div>
+    <br>
+    <div class="sidebar videos">
+        <div class="section-header item-header desktop">
+            <div class="title">
+                <h1>Hip Hop
+                    <span>Videos</span>
+                </h1>
+            </div>
+        </div>
+        <table>
+            <tbody>
+                <tr>
+                    <td><a href="{{ route('posts.show',$post->slug) }}">
+                        <img src="https://static.hiphopdx.com/2020/08/15913946374530134-511-220x165.png" width="75" height="75">  
+                    </a></td>
+                    <td>
+                        <p class="title">
+                            <a href="{{ route('posts.show',$post->slug) }}">Rico Nasty</a>
+                        </p>
+                        <p class="excerpt">iPhone</p>
+                    </td>
+                    
+                </tr>
+            </tbody>
+        </table>
+        <a href="/category/video" class="uni-link">
+            More Videos &nbsp;
+            <i class="fa fa-angle-right"></i>
+        </a>
+    </div>
+    <br>
+    <div class="sidebar reviews">
+        <div class="section-header item-header desktop">
+            <div class="title">
+                <h1>Reviews 
+                    <span></span>
+                </h1>
+            </div>
+        </div>
+        <ul class="vidlist">
+            <li class="clearfix">
+                <a href="{{ route('posts.show',$post->slug) }}">
+                    <img src="https://static.hiphopdx.com/2020/07/Logic_No_Pressure_album_cover-165x165.jpeg" alt="">
+                </a>
+                <a href="{{ route('posts.show',$post->slug) }}">
+                    <img src="https://static.hiphopdx.com/2020/07/109268521_3763077460376160_2370091438898317920_n-165x165.jpg" alt="">
+                </a>
+            </li>
+        </ul>
+        <a href="/category/reviews" class="uni-link">
+            More Reviews &nbsp;
+            <i class="fa fa-angle-right"></i>
+        </a>
+    </div>
+    <div class="desktop side-ad">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Ads Resp 1 -->
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-8897835908934169"
+            data-ad-slot="2316934125"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
+    </div>
