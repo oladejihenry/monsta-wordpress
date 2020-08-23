b7f2716848b81823c68efa01437d337cc4a4f468
Henry Oladeji <officialmonstajamss@gmail.com>
2020-08-23 18:44:03 +0100

    Minor Update

--- index.php
+++ index.php
@@ -2,47 +2,6 @@
 <?php get_sidebar(); ?>  
 <div class="content home  with-sidebars">
     <div class="just-homepage">
-        <div id="newsbox" class="desktop">
-            <div class="section-header item-header desktop">
-                <div class="title">
-                    <h2>News:
-                        <span>Today's Headline</span>
-                    </h2>
-                </div>
-            </div> 
-            <div class="newscol right">
-                <ul class="list">
-                    <li><a href="{{ route('posts.show',$post->slug) }}">Eminem - Rap God</a></li>
-                </ul>
-            </div>
-            <div class="newscol">
-                <ul class="list">
-                    <li><a href="{{ route('posts.show',$post->slug) }}">50 Cent - In Da Club</a></li>
-                </ul>
-            </div>
-            <div class="clear"></div>
-            <a href="/category/news" class="uni-link">
-                More News Stories &nbsp;
-                <i class="fa fa-angle-right"></i>
-            </a>
-        </div>
-        <div class="box-list small desktop">
-            <div class="section-header item-header desktop">
-                <div class="title">
-                    <h2>MonstaJams
-                        <span>Exclusives</span>
-                    </h2>
-                </div>
-            </div>
-            <div class="story">
-                <a href="{{ route('posts.show',$post->slug) }}">
-                    <img src="https://static.hiphopdx.com/2020/07/200726-Eric-B-Rakim-150x150.jpg">
-                </a>
-                <p class="title">
-                    <a href="{{ route('posts.show',$post->slug) }}">Eminem - Rap God</a>
-                </p>
-            </div>
-        </div> 
         <div class="section-header item-header desktop">
             <div class="title">
                 <h2><?php echo get_bloginfo('name'); ?>
