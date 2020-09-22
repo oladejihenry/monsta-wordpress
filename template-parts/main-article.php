a7596ab52466322a187868002c403461ad4ee470
Henry Oladeji <officialmonstajamss@gmail.com>
2020-09-22 21:01:11 +0100

    Minor Update

--- template-parts/main-article.php
+++ template-parts/main-article.php
@@ -4,7 +4,8 @@
     |
     <?php echo meks_time_ago(); ?>
     </span> 
-    <img class="post-image rectangle" src="<?php the_post_thumbnail_url('thumbnail'); ?>"> 
+	<?php $thumb_id = get_post_thumbnail_id(get_the_ID()); $alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true); ?>
+    <img class="post-image rectangle" src="<?php the_post_thumbnail_url('thumbnail'); ?>" <?php if ($alt): ?> alt="<?php echo $alt; ?>" <?php endif; ?> /> 
     <h3 class="title"><?php the_title(); ?></h3>
     <?php echo '<p class="excerpt">'. get_the_excerpt() .'</p>';?>
     <div class="post-meta"> 
